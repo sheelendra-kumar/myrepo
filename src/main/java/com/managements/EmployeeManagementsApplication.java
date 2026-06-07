2912173a7136a64ea5ac709f84b04c747df78306
Pravendra Kumar <pravendra.kumar@outlook.com>
2026-06-07 10:42:11 +0530

    Initial Commit

---
+++ src/main/java/com/managements/EmployeeManagementsApplication.java
@@ -0,0 +1,13 @@
+package com.managements;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeManagementsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeManagementsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/managements/EmployeeManagementsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.managements;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeManagementsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
